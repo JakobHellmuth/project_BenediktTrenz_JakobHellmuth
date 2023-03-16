77f265b7ece4866df5cb3daf54da88b5e96734b6
Trenzbenedikt <122484538+benedikttrz@users.noreply.github.com>
2023-03-16 10:53:29 +0000

    adding developer.c
    editing developer.c

--- Project/src/developer.c
+++ Project/src/developer.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include "developer.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void developer_init(developer *dev, const char *name, const char *alias)
+{
+strncpy(dev->name, name, strlen(name) +1);
+strncpy(dev->alias, alias, strlen(alias) +1);
+}
