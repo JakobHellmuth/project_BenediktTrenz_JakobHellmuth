add50a4c2d446e0ff1228b1191f17e2a141d1731
Trenzbenedikt <122484538+benedikttrz@users.noreply.github.com>
2023-03-16 10:17:22 +0000

    editing developer.h
    defining debveloper struct

--- Project/include/developer.h
+++ Project/include/developer.h
@@ -1,1 +1,16 @@
-#include <stdio.h>
+#ifndef DEVELOPER H 2888273536
+#define DEVELOPER H 2888273536
+
+#define NAME_LENGTH (25U)
+#define ALIAS_LENGTH (15U)
+
+typedef struct
+{
+char name[NAME_LENGTH];
+char alias[ALIAS_LENGTH];
+}
+developer;
+
+void developer_init(developer *dev, const char *name, const char *alias);
+
+#endif
