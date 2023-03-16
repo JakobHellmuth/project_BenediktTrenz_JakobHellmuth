5bebbb23f220540ae31af28351afafc9c552036c
JakobHellmuth <122924872+JakobHellmuth@users.noreply.github.com>
2023-03-16 14:36:28 +0000

    adding cmake_run

---
+++ Project/build/CMakeFiles/Project_exec.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/project_info/Project/src/developer.c" "CMakeFiles/Project_exec.dir/src/developer.c.o" "gcc" "CMakeFiles/Project_exec.dir/src/developer.c.o.d"
+  "/workspaces/project_info/Project/src/developer_group.c" "CMakeFiles/Project_exec.dir/src/developer_group.c.o" "gcc" "CMakeFiles/Project_exec.dir/src/developer_group.c.o.d"
+  "/workspaces/project_info/Project/src/main.c" "CMakeFiles/Project_exec.dir/src/main.c.o" "gcc" "CMakeFiles/Project_exec.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Project/build/CMakeFiles/Project_exec.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_exec.dir/src/developer.c.o"
+  "CMakeFiles/Project_exec.dir/src/developer.c.o.d"
+  "CMakeFiles/Project_exec.dir/src/developer_group.c.o"
+  "CMakeFiles/Project_exec.dir/src/developer_group.c.o.d"
+  "CMakeFiles/Project_exec.dir/src/main.c.o"
+  "CMakeFiles/Project_exec.dir/src/main.c.o.d"
+  "Project_exec"
+  "Project_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
