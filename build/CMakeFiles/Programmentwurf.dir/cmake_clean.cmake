7eac95ec540d3693cfad68abed41d8a1b2945d9c
JakobHellmuth <122924872+JakobHellmuth@users.noreply.github.com>
2023-03-16 10:27:29 +0100

    Initial commit

---
+++ build/CMakeFiles/Programmentwurf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programmentwurf.dir/Programmentwurf.c.o"
+  "CMakeFiles/Programmentwurf.dir/Programmentwurf.c.o.d"
+  "Programmentwurf"
+  "Programmentwurf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Programmentwurf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Programmentwurf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Programmentwurf.
+# This may be replaced when dependencies are built.
