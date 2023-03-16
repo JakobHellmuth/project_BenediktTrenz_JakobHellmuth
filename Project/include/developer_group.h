6c209e3db6120bb16337ada6d69fd5912e7e139b
JakobHellmuth <122924872+JakobHellmuth@users.noreply.github.com>
2023-03-16 10:30:09 +0000

    editing developer_group.h
    defining struct developer_group and adding function developer_group_init

--- Project/include/developer_group.h
+++ Project/include/developer_group.h
@@ -1,2 +1,20 @@
-#include <stdio.h>
+#ifndef DEVELOPER_GROUP_H_8680918198
+#define DEVELOPER_GROUP_H_8680918198
+
 #include "developer.h"
+#include <stdio.h>
+
+#define LOGO_LENGTH (3000U)
+#define DEV_1 (75U)
+#define DEV_2 (75U)
+
+typedef struct
+{
+	developer dev1[DEV_1];
+	developer dev2[DEV_2];
+	char logo[LOGO_LENGTH];
+} developer_group;
+
+void developer_group_init(developer_group* d_g, const developer* dev1, const developer* dev2, const char* logo);
+
+#endif
