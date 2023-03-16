15ce7778eb553b4c3610b677334d88d97b06cb33
JakobHellmuth <122924872+JakobHellmuth@users.noreply.github.com>
2023-03-16 11:30:46 +0000

    fixing developer.c

--- Project/src/developer.c
+++ Project/src/developer.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <stdio.h>
 #include "developer.h"
 
@@ -11,17 +9,4 @@ void developer_init(developer *dev, const char *name, const char *alias)
 strncpy(dev->name, name, strlen(name) +1);
 strncpy(dev->alias, alias, strlen(alias) +1);
 }
-=======
-#include "developer_group.h"
-#include <string.h>
 
-	void developer_group_init(developer_group* d_g, const developer* dev1, const developer* dev2, const char* logo)
-	{
-		strncpy((char *)d_g->dev1,(char*) dev1, strlen((char*)dev1) +1);
-		strncpy((char *)d_g->dev2,(char*) dev2, strlen((char*)dev2) +1);
-		strncpy(d_g->logo, logo, strlen(logo) +1);
-	}
->>>>>>> editing_developer_group
-=======
-
->>>>>>> editing_developer_group
