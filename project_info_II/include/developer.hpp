9466bc68e4edcb0c9b6032a0f6cf9856e6ef7733
JakobHellmuth <122924872+JakobHellmuth@users.noreply.github.com>
2023-06-01 15:49:12 +0000

    create developer.hpp
    defining class Developer an constructors. Defining functions

---
+++ project_info_II/include/developer.hpp
@@ -0,0 +1,50 @@
+#ifndef DEVELOPER_HPP
+#define DEVELOPER_HPP
+
+#include <iostream>
+#include <memory>
+
+class Developer
+{
+protected:
+    std::string name;
+    std::string alias;
+
+public:
+    // Constructor
+    Developer(const std::string& name, const std::string& alias);
+
+    // Static method
+    static void drink_beer();
+
+    // Getter methods
+    std::string getName() const;
+    std::string getAlias() const;
+
+    // Pure virtual method
+    virtual void solve_problem() = 0;
+
+    // Friend function for stream output
+    friend std::ostream& operator<<(std::ostream& os, const Developer& developer);
+};
+
+class JuniorDeveloper : public Developer
+{
+public:
+    using Developer::Developer;
+
+    // solve_problem method
+    void solve_problem() override;
+};
+
+class SeniorDeveloper : public Developer
+{
+public:
+    using Developer::Developer;
+
+    // Override solve_problem() method
+    void solve_problem() override;
+};
+
+#endif
+
