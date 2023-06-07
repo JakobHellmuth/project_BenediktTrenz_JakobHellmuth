8aa3ea4a162c913a47f7c290f1a02f651a88683e
JakobHellmuth <122924872+JakobHellmuth@users.noreply.github.com>
2023-06-07 08:24:14 +0000

    deleting build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/project_info/project_info_II
-# Build directory: /workspaces/project_info/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/workspaces/project_info/build/developer_tests[1]_include.cmake")
--- project_info_II/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/project_info/project_info_II
-# Build directory: /workspaces/project_info/project_info_II/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/workspaces/project_info/project_info_II/build/developer_tests[1]_include.cmake")
